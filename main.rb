e9f2d8214951c70838d0a465d79b0b5541c1701a
pocari <caffelattenonsugar@gmail.com>
2023-03-08 03:28:53 +0900

    first commit

---
+++ main.rb
@@ -0,0 +1,35 @@
+require 'openai'
+
+class OpenAIClient
+  def initialize
+    @client = OpenAI::Client.new(
+      access_token: ENV['OPEN_API_SECRET_KEY']
+    )
+    @messages = []
+  end
+
+  def chat(prompt)
+    response = @client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: @messages + [{ role: "user", content: prompt }],
+      })
+    add_message('user', prompt)
+    # p [:response, response]
+    ans = response.dig("choices", 0, "message")
+    ans['content']&.strip.tap do |content|
+      add_message(ans['role'], content)
+    end
+  end
+
+  def add_message(role, content)
+    @messages << { role: role, content: content }
+  end
+end
+
+client = OpenAIClient.new
+
+loop do
+  print('prompt> ');
+  puts client.chat(gets.chomp)
+end
